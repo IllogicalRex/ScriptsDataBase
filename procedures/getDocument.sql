6e44fb4c8991d4d0028de5af3bfd00d8fec1a544
Josue <e-jjmartinezt@neoris.com>
2019-12-09 13:31:50 -0700

    Get documento por usuario

--- procedures/getDocument.sql
+++ procedures/getDocument.sql
@@ -6,12 +6,24 @@ create proc getDocumento(@AlumnoNumControl int)
 			Select * FROM Documento WHERE @AlumnoNumControl=AlumnoId
 		END
 
--- Mostrar documentos del alumno asignado al asesor
+--Mostrar los documentos del asesor en las fases 3,4,5,7
 go
-create proc getDocumentoAlumnoAsesor(@AsesorId varchar(20))
+alter proc getDocumentoAlumnoAsesor(@AsesorId varchar(20),@repor1 int,@repor2 int ,@repor3 int,@reporFinal int)
 	AS 
 		BEGIN
-			Select * FROM Documento WHERE @AsesorId=Idasesor
+		Select * FROM Documento WHERE @AsesorId=Idasesor AND (@repor1=Idtipo OR @repor2=Idtipo OR @repor3=Idtipo OR @reporFinal=Idtipo)
 		END
 
 go
+--Mostrar los documentos del encargado en las fases 2,6,7
+alter proc getDocumentoEncargado(@EncargadoId varchar(20),@tipoAnte int,@tipoSegui int ,@tipoReport int)
+	AS 
+		BEGIN
+			Select * FROM Documento WHERE @EncargadoId=idadmin AND (@tipoAnte=Idtipo OR @tipoSegui=Idtipo OR @tipoReport=Idtipo)
+		END
+
+go
+
+--exec getDocumentoAlumnoAsesor '12345678' , 3,4,5,7
+
+
